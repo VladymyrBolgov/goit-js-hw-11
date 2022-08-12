d4101c0ffe85b398a84258640683a3279ba12159
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-08-13 02:10:24 +0300

    k

--- src/js/fetchGallery.js
+++ src/js/fetchGallery.js
@@ -1,11 +1,10 @@
 const axios = require("axios").default;
 
 const fetchGallery = async () => {
-    try {
-      const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
-      createList(response.date);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  fetchGallery();
+  const response = await axios.get(
+    "https://pixabay.com/api/?key=29230094-1d6fe7151785ccfc3d660c9e4&q=${value}&image_type=photo&orientation=horizontal&safesearc=true&per_page=40&page=${step}"
+  );
+  return response;
+};
+  
+export default fetchGallery;
