6554014675763cf161f33e311f7b5cc8732aaea3
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-08-16 21:11:51 +0300

    п

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,3 @@
-/*import { formEl, loadMoreBtnEl } from './ref';
-import onClickLonBtnSubmit from './onClickLonBtnSubmit';
-import { onClickLonBtnSubmit, onClickAddPage } from './onClickLonBtnSubmit';
-
-formEl.addEventListener('submit', onClickLonBtnSubmit);
-loadMoreBtnEl.addEventListener('click', onClickAddPage);*/
-
-
-
 
 
 import axios from "axios";
